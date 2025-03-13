be96f8720ed9f317438685f3bd40d99a2663c286
AkhenatonBR87 <brasilakhenaton@gmail.com>
2025-03-13 20:05:46 -0300

    Create pinheiro-07.py

---
+++ pinheiro-07.py
@@ -0,0 +1,14 @@
+#7. Pinheiro  - Implemente um programa que desenhe um "pinheiro" na tela, similar ao abaixo.Enriqueça o desenho com outros caracteres, simulando enfeites.
+
+print("        ***")
+print("         X")
+print("       oXXXo")
+print("      *XXXXX*")
+print("     oXXXXXXXo")
+print("    *XXXXXXXXX*")
+print("   oXXXXXXXXXXXo")
+print("  *XXXXXXXXXXXXX*")
+print(" oXXXXXXXXXXXXXXXo")
+print("        XX")
+print("        XX")
+print("       XXXX")
